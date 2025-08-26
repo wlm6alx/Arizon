600b98e1249ae2cd61706d98e7ffe76a5a48f839
Alex <alexwillem30@gmail.com>
2025-08-27 00:33:18 +0100

    Header is okay let's go to the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Header/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
